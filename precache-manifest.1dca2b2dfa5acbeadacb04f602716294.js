b8b03712aecb1321a228009a165835d30fb5fb79
Yerassyl Kurmangali <theycallmeinvo@gmail.com>
2021-06-28 16:56:19 +0600

    Updates

---
+++ precache-manifest.1dca2b2dfa5acbeadacb04f602716294.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c20fe3a27a6c90e5c246c979d7ecad9",
+    "url": "/kindergarten/index.html"
+  },
+  {
+    "revision": "2e0a8069df39df6bf752",
+    "url": "/kindergarten/static/css/main.3a60b056.chunk.css"
+  },
+  {
+    "revision": "65b66186bd33f9892d05",
+    "url": "/kindergarten/static/js/2.5c7cecec.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/kindergarten/static/js/2.5c7cecec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e0a8069df39df6bf752",
+    "url": "/kindergarten/static/js/main.796aed0d.chunk.js"
+  },
+  {
+    "revision": "6bc40f711e2542e4b946",
+    "url": "/kindergarten/static/js/runtime-main.d2d3ebf6.js"
+  },
+  {
+    "revision": "0f6e082f42f5247d82c3ade9c2a8f928",
+    "url": "/kindergarten/static/media/Gilroy-ExtraBold.0f6e082f.otf"
+  },
+  {
+    "revision": "31f9f2318442873588fcab7474ecc987",
+    "url": "/kindergarten/static/media/Gilroy-ExtraBold.31f9f231.woff"
+  },
+  {
+    "revision": "36f988ad198db36aaac7e1fbac876d34",
+    "url": "/kindergarten/static/media/Gilroy-ExtraBold.36f988ad.woff2"
+  },
+  {
+    "revision": "48f99c5d06ef1101bddfb2e4c7dd4889",
+    "url": "/kindergarten/static/media/Gilroy-Light.48f99c5d.woff2"
+  },
+  {
+    "revision": "554c6ae491829b842eac7bcc047cd3a7",
+    "url": "/kindergarten/static/media/Gilroy-Light.554c6ae4.woff"
+  },
+  {
+    "revision": "c62aded729bf7146d491275e5019d7fc",
+    "url": "/kindergarten/static/media/Gilroy-Light.c62aded7.otf"
+  },
+  {
+    "revision": "c240bc0fde52cd039087c23f9b63eab5",
+    "url": "/kindergarten/static/media/Untitled.c240bc0f.jpg"
+  },
+  {
+    "revision": "ade6b88d142a6534dbaf0914e94a3513",
+    "url": "/kindergarten/static/media/aboutUsCover.ade6b88d.jpg"
+  },
+  {
+    "revision": "949b246ced5e01b61cbdd4cbf97fbefc",
+    "url": "/kindergarten/static/media/asf.949b246c.jfif"
+  },
+  {
+    "revision": "0902b4bb5e457b50be8ba18e04b951f4",
+    "url": "/kindergarten/static/media/beige.0902b4bb.jfif"
+  },
+  {
+    "revision": "49b96578ee9b2e95d699193f27732d1d",
+    "url": "/kindergarten/static/media/instagram.49b96578.svg"
+  },
+  {
+    "revision": "fc76036692f5ec5a1817207edb11854e",
+    "url": "/kindergarten/static/media/ipadCover.fc760366.jpg"
+  },
+  {
+    "revision": "853605df4ec6d49ccff35e44e49f961c",
+    "url": "/kindergarten/static/media/logo.853605df.jpg"
+  },
+  {
+    "revision": "b6358c03359f36c4e63e87f79fc34df7",
+    "url": "/kindergarten/static/media/mainpicture.b6358c03.jfif"
+  },
+  {
+    "revision": "78c0bfd40536143dcc142854dd8627a7",
+    "url": "/kindergarten/static/media/mobileCover.78c0bfd4.jpg"
+  },
+  {
+    "revision": "0e7f03fbea21d8ab25eddf2604d5f53c",
+    "url": "/kindergarten/static/media/paragraphcover.0e7f03fb.jpg"
+  },
+  {
+    "revision": "02ab9b9e029dddddc7180cc9ff1b1d22",
+    "url": "/kindergarten/static/media/whatsapp.02ab9b9e.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7169e60cf8db5ca00c7d7ceeea304960.js"
+  "/kindergarten/precache-manifest.1dca2b2dfa5acbeadacb04f602716294.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/kindergarten/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
